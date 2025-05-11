64242b44cecd1cc5d4ff5355ce99253435abf25c
김강현 <skjy7200@gmail.com>
2025-05-11 22:53:20 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>6주차 과제 - 김강현 /kanghyun 에서 확인</div>} />
+        <Route path="/" element={<div>기본 페이지. /kanghyun 으로 이동.</div>} />
         <Route path="/kanghyun" element={<Kanghyun />} />
       </Routes>
     </Router>
