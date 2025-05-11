334d1072acf64362e35c8259f2f89eb669f12c43
김강현 <skjy7200@gmail.com>
2025-05-11 22:05:38 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Kanghyun from "./assignments/kanghyun";
+
+<Route path="/kanghyun" element={<Kanghyun />} />
+
 
 /**
  * 과제1-2: 이곳에 assignments 폴더 내 작성한 코드를 불러오는 코드를 작성해주세요.
