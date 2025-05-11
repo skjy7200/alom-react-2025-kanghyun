10216ffa0e48771d791aeee1f66e7dd3b2238b4f
김강현 <skjy7200@gmail.com>
2025-05-11 22:14:04 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Kanghyun from "./assignments/kanghyun";
 import Example from "./assignments/example";
 
-/**
- * 과제1-2: 이곳에 assignments 폴더 내 작성한 코드를 불러오는 코드를 작성해주세요.
- * 예시:
- * import Woojin from "./assignments/woojin";
- */
-import Example from "./assignments/example";
-
 function App() {
   return (
     <Router>
       <Routes>
-        {/* 과제1-2: 이곳에 과제1-1에서 작성한 코드를 불러오는 코드를 작성해주세요.
-            ex) <Route path="/woojin" element={<Woojin />} />
-        */}
         <Route
           path="/"
-          element={<div>App.js와 example.js를 확인해주세요.</div>}
+          element={<div>6주차 과제-김강현</div>}
         />
         <Route path="/example" element={<Example />} />
+        <Route path="/kanghyun" element={<Kanghyun />} /> 
       </Routes>
     </Router>
   );
